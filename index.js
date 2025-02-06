609e3bb406c6660afcd44b6dcddc62788647a22d
CodeSadiq <sadiq.imam404@gmail.com>
2025-02-06 16:32:40 +0530

    modify html css and js file

---
+++ index.js
@@ -0,0 +1,4 @@
+for (let i=0 ; i<14; i++){
+    document.querySelectorAll(".songName")[i].textContent= document.querySelectorAll(".songNameinCard")[i].textContent;
+    document.querySelectorAll(".artistName")[i].textContent= document.querySelectorAll(".artistNameinCard")[i].textContent;
+    }
