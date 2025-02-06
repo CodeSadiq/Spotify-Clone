ee921e9db6318e723da9ca7f13893a2ba8a1270a
CodeSadiq <sadiq.imam404@gmail.com>
2025-02-06 18:54:08 +0530

    update html css files

--- index.js
+++ index.js
@@ -2,3 +2,7 @@ for (let i=0 ; i<14; i++){
     document.querySelectorAll(".songName")[i].textContent= document.querySelectorAll(".songNameinCard")[i].textContent;
     document.querySelectorAll(".artistName")[i].textContent= document.querySelectorAll(".artistNameinCard")[i].textContent;
     }
+let songArray = [];
+for (let i= 0; i<14; i++){
+    songArray.push(new Audio(`./songs/${i+1}.mp3`))
+}
