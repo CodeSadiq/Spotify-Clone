ce776a90aa04850517327ed071ddd2a39abd1506
CodeSadiq <sadiq.imam404@gmail.com>
2025-02-08 22:58:08 +0530

    update html css and js

--- index.js
+++ index.js
@@ -1,8 +1,30 @@
+let songArray = [];
+let audio;
+let audioIsPlaying = false;
 for (let i=0 ; i<14; i++){
     document.querySelectorAll(".songName")[i].textContent= document.querySelectorAll(".songNameinCard")[i].textContent;
     document.querySelectorAll(".artistName")[i].textContent= document.querySelectorAll(".artistNameinCard")[i].textContent;
-    }
-let songArray = [];
-for (let i= 0; i<14; i++){
-    songArray.push(new Audio(`./songs/${i+1}.mp3`))
-}
+    document.querySelectorAll(".librarySongList")[i].classList.add(`${i+1}`);
+    songArray.push(new Audio (`./songs/${i+1}.mp3`));
+};
+
+$(".card").on("click" , function() {
+    if( audioIsPlaying=== true){
+        audio.currentTime =0;
+        audio.pause();
+    };
+   songArray[this.classList[0]-1].play();
+   audio= songArray[this.classList[0]-1]
+   audioIsPlaying = true;
+   $().attr("src", "./icon/pause.png");
+});
+
+$(".librarySongList").on("click" , function() {
+    if(audioIsPlaying === true){
+        audio.currentTime =0;
+        audio.pause();
+    };
+    songArray[this.classList[1]-1].play();
+    audio= songArray[this.classList[1]-1]
+    audioIsPlaying = true;
+ });
